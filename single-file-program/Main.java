c11de516b4dc54463030279e8b9600479ee7b2a0
Edwin Garcia <egarciavalle2014@gmail.com>
2024-01-30 16:18:20 -0600

    docs: fix typo in literal text block
    Signed-off-by: Edwin Garcia <egarciavalle2014@gmail.com>

--- single-file-program/Main.java
+++ single-file-program/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         var input = """
-        Hel o World,
+        Hello World,
         this is a poem,
         for the girl I like.
         """;

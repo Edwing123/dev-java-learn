edebbacf150a2ba714fd9fec51745e70218eb243
Edwin Garcia <egarciavalle2014@gmail.com>
2024-02-21 03:25:00 -0600

    chore: create initial file for next lesson
    Signed-off-by: Edwin Garcia <egarciavalle2014@gmail.com>

---
+++ numbers-and-strings/Strings.java
@@ -0,0 +1,13 @@
+/**
+  *
+  * @author Edwing123
+  * 
+  * The [String] class.
+  */
+
+public class Strings {
+  public static void main(String... args) {
+    System.err.println("Working with the [String] class.");
+  }
+}
+

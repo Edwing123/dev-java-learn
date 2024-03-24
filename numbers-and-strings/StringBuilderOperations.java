b69c851440c054acb1387ca5ce548e816a5410f1
Edwin Garcia <egarciavalle2014@gmail.com>
2024-03-23 19:38:28 -0600

    chore: create initial file to learn about SB operations
    SB = [StringBuilder].
    
    Signed-off-by: Edwin Garcia <egarciavalle2014@gmail.com>

---
+++ numbers-and-strings/StringBuilderOperations.java
@@ -0,0 +1,14 @@
+
+/**
+*/
+
+public final class StringBuilderOperations {
+  void main() {
+    println("String builder operations.");
+  }
+
+  static void println(Object obj) {
+    System.err.printl(obj);
+  }
+}
+
